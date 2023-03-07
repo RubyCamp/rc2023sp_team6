10546a3a1f89c3d62a1bb7e982c216ca0d9e03af
Uenaka Fuka <21241030@kyoto-wu.ac.jp>
2023-03-07 11:49:07 +0900

    サンプルプログラムをコピーしました

--- main.rb
+++ main.rb
@@ -1,1 +1,22 @@
-puts "Hello World!"
+require 'dxruby'
+
+require_relative 'image_manager'
+require_relative 'clicked_anim'
+
+# 依存するファイル群を一括でrequireする
+Dir.glob("directors/*.rb") {|path| require_relative path }
+Dir.glob("map_chips/*.rb") {|path| require_relative path }
+
+require_relative 'map'
+
+Window.width = 800
+Window.height = 600
+Window.caption = "RubyCamp 2023SP Sample Game"
+
+# ゲーム開始時点で実行する最初のシーンディレクターを生成
+current_director = Directors::Title.new
+
+Window.loop do
+	current_director = current_director.play
+	break if Input.key_push?(K_ESCAPE) # ESCキー押下でメインループを抜ける
+end
